89f6808101762848db0ccf5c5d41b96b587dc5c8
Abel Fernandez <abelfergodevelop@gmail.com>
2020-02-01 22:24:50 +0100

    Add enable Feign clients

--- target/classes/archetype-resources/__rootArtifactId__-service/src/main/java/service/MainServiceApplication.java
+++ target/classes/archetype-resources/__rootArtifactId__-service/src/main/java/service/MainServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan("${symbol_dollar}{abeldevelop.configuration.entity-scan}")
 @EnableJpaRepositories("${symbol_dollar}{abeldevelop.configuration.enable-jpa-repositories}")
 @EnableDiscoveryClient
+@EnableFeignClients({"${symbol_dollar}{abeldevelop.configuration.component-scan}"})
 @SpringBootApplication
 public class MainServiceApplication {
 
